c8bb500593d705dfb6368fdc64e449178ea0099b
Vinicius <viniciusdesouto@gmail.com>
2021-07-28 11:20:06 -0300

    init project

---
+++ src/components/SearchBook/index.tsx
@@ -0,0 +1,13 @@
+import { Container } from './styles';
+
+export function SearchBook() {
+  return (
+    <Container>
+      <input 
+        placeholder="Search book" 
+      />
+
+      <h2>Hello, Vinícius de Souto!</h2>
+    </Container>
+  )
+}
---
+++ src/components/SearchBook/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  input {
+    margin-top: 3.125rem;
+    margin-left: 1.25rem;
+
+    background: var(--input);
+    width: 21rem;
+    height: 3rem;
+    color: #54565A;
+    font-size: 1rem;
+    font-weight: 1.125rem;
+    font-family: 'Poppins', sans-serif;
+    border-radius: 0.675rem;
+    border: none;
+    box-shadow: 5px 5px 80px #D4AD86 rgba(212, 173, 134, 0.122623);
+  }
+`;
